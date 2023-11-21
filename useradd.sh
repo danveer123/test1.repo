dcf02002e67f687a6bb6f1f4895af0b4e8cb2c04
root <root@demo.com>
2023-11-21 15:29:51 +0530

    user add script

---
+++ useradd.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+## script to add new user 
+read -p "give your user name : " a
+b=`grep "^$a" /etc/passwd | cut -d ':' -f1`
+if [[ "$a" == "$b" ]]
+then
+	echo " $a is exist"
+	exit 1
+else 
+useradd $a
+read -p "give user password : " c
+echo $c | passwd --stdin $a
+id $a
+	
+fi
+
+
